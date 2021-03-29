4014faf6fc47bad21a7e93524175c964e97ca378
Rafael <dev@rafaelantun.es>
2021-03-29 16:39:13 -0300

    json integration + added features

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
+import Button from "./Button"
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, buttonText}) => {
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <button className='btn'>Add</button>
-
+            <Button color={buttonText ? 'red' : 'green'} text={buttonText ? "Hide" : "Add"} onClick={onAdd}/>
         </header>
     )
 }
