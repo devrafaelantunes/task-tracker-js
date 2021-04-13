8697d14a87c04aa5d8c0cd3702b37db33b93411f
Rafael Antunes <78612579+devrafaelantunes@users.noreply.github.com>
2021-04-13 10:15:25 -0300

    tasks count now added

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,6 +5,8 @@ const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         <div>
             {tasks == undefined ? "Starting App" : tasks.length == 0 && <p>No tasks to show</p>}
+            {tasks == undefined ? "Starting App" : tasks.length > 0 &&
+            <p style={{margin: '0, auto'}}>You completed: {tasks[0].task_count} tasks.</p>}
             {tasks == undefined ? console.log("Starting app") : tasks.map((task, index) =>
             (<Task
                 task={task}
